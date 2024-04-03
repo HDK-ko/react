1d3b706e8b749b7ef0164b4abf232d9b8eacb231
DESKTOP-NLG2FOM\c <lakeibt15@gmail.com>
2024-04-03 22:28:02 +0900

    20240403

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component, useState } from 'react';
-
-const SplitMe = React.lazy(()=> import('./SplitMe'));
+import React, { Component, Suspense, useState } from 'react';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
   return (
     <div className='App'>
-      
+      <header className='App-header'>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
+        {visible && <SplitMe/>}
+      </header>
     </div>
   )
 }
